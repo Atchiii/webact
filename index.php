9eeffd31e37368f968419be6b94229a83abc525b
Atchiii <143099732+Atchiii@users.noreply.github.com>
2023-10-31 22:32:12 +0800

    created index and table

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Subject Scheduling</title>
+</head>
+<body>
+    <h1>My Subjects</h1>
+
+    <table width='100%' border=1>
+        <tr>
+            <td>ID</td>
+            <td>Subject Code</td>
+            <td>Subject Name</td>
+            <td>Action</td>
+        </tr>
+    </table>
+</body>
+</html>
