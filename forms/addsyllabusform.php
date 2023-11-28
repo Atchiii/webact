0382aa96d3c31ac9940a6e5caeafd4407fc3325e
Atchiii <143099732+Atchiii@users.noreply.github.com>
2023-11-29 02:25:16 +0800

    added forms and can now add info

---
+++ forms/addsyllabusform.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+    <title>Add Syllabus</title>
+</head>
+<body>
+    <h1>Add Syllabus</h1>
+    <a href="../index.php">Home</a>
+    <br>
+    <br>
+    <form action="../functions/addsyllabus.php" method="post" name="form1">
+        <table width="25%">
+            <tr>
+                <td>Syllabus Code</td>
+                <td><input type="text" name="code"></td>
+            </tr>
+            <tr>
+                <td>Syllabus Author</td>
+                <td><input type="text" name="author"></td>
+            </tr>
+            <tr>
+                <td>Subjects</td>
+                <td>
+                    <select name="subject">
+                        <?php
+                            include_once("../dbConnecttion\mysql_config_connection.php");
+                            $query = "SELECT * FROM tbls";
+                            $result = mysqli_query($dbc, $query);
+                            while($res = mysqli_fetch_array($result)){
+                                echo "<option value=\"".$res['Subject_id']."\">";
+                                echo $res ['Subject_name'];
+                                echo "</option>";
+                            }
+                        ?>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name='Submit' value="Add"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
--- index.php
+++ index.php
@@ -10,7 +10,8 @@
 </head>
 <body>
     <h1>My Subjects</h1>
-    <a href="forms/addform.php">Add Subject</a><br/><br/>
+    <a href="forms/addform.php">Add Subject</a>
+    <a href="forms/addsyllabusform.php">Add Syllabus</a><br/><br/>
     <table width='100%' border=1>
         <tr bgcolor = "#CCCCCC">
             <td>ID</td>
